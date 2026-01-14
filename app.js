bea61ae8aeb7d77ea4cdf077683965bdeb448457
Sourabh Patole <sourabhpatole@outlook.com>
2026-01-14 09:49:23 +0530

    nested require added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // require("./xyz");
 
 // const obj = require("./sum");
-const { calculateSum, x } = require("./sum");
+// const { calculateSum, x } = require("./calculate/sum");
+// const { calculateMultiply } = require("./calculate/multiply");
+
+// nested import method
+const { calculateMultiply, calculateSum, x } = require("./calculate");
 // import { calculateSum } from "./sum.js";
 var fname = "Namaste Node JS";
 var a = 10;
@@ -12,4 +16,5 @@ console.log(a + b);
 // console.log(global);
 // console.log(this);
 calculateSum(2, 4);
-// console.log(x);
+calculateMultiply(4, 2);
+console.log(x);
---
+++ calculate/multiply.js
@@ -0,0 +1,5 @@
+function calculateMultiply(a, b) {
+  let mul = a * b;
+  console.log(mul);
+}
+module.exports = { calculateMultiply };
