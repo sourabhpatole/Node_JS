a18cd83aae0b4735cde55b9be96e8b5898f29a86
Sourabh Patole <sourabhpatole@outlook.com>
2026-01-15 17:40:27 +0530

    comment added

--- blocking.js
+++ blocking.js
@@ -3,9 +3,10 @@ console.log("Hello world");
 
 let a = 99;
 let b = 43;
-//
+//synchronous function will block the main thread DON"t use IT
 crypto.pbkdf2Sync("password", "salt", 5000000, 50, "sha512");
 console.log("first key is generated");
+// async function
 crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
   console.log("second key is genereated");
 });
